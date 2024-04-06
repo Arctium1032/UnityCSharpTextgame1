e14899148a11eda29c6eae450c4d79d236e21dcb
Arctium1032 <dhfksdlrlt@naver.com>
2024-04-06 16:24:26 +0900

    20240406
    20240406

---
+++ My project/Assets/script/C# study/Hello Unity.cs
@@ -0,0 +1,20 @@
+using System.Collections;   // ~ system을 사용하겠다. system namespace를 사용하겠다.
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloUnity : MonoBehaviour
+{
+    //List<int> list = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Unity");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ My project/namespacetest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace
+{
+    internal class program
+    {
+       
+        static void main(string[] args)
+        {
+           
+            Console.WriteLine("Hello, World");
+
+            Console.WriteLine("안녕하세요");
+        
+        }
+
+    }
+}
