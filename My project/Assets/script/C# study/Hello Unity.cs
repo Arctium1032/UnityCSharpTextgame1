3f250ff7326b3332925d259ec3b1e192a339580e
Arctium1032 <dhfksdlrlt@naver.com>
2024-04-06 17:27:54 +0900

    20240406
    변수작성

--- My project/Assets/script/C# study/Hello Unity.cs
+++ My project/Assets/script/C# study/Hello Unity.cs
@@ -1,20 +1,24 @@
-using System.Collections;   // ~ system을 사용하겠다. system namespace를 사용하겠다.
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class HelloUnity : MonoBehaviour
 {
-    //List<int> list = new List<int>();
+    public TMPro.TextMeshProUGUI text; // 데이터 타입인지 모르겠지만 사용하기 위해서 text이름의 변수로 지정
 
-    // Start is called before the first frame update
-    void Start()
+    int damage = 100;           // 정수값
+    float time = 12.4f;         // 실수값
+    string name = "주인공";     // 문자열 값
+
+    private void Start() // 게임 시작 후 한번만 실행
     {
-        Debug.Log("Hello Unity");
+        Debug.Log("데미지의 값" + damage);
+        Debug.Log("시간의 값" + time);
+        Debug.Log("이름" + name);
+
+
+        text.text = string.Format($"Only {damage} english {time} is visible? : 정말이니 ?"); // 갈길이 멀다
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
